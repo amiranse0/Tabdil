0671d08a2e7196504e6f325bf2c42096d4b80a76
amirabbas <amiranse0@gmail.com>
2023-04-18 03:39:27 +0430

    dependency injection started, app module and app class added.

---
+++ app/src/main/java/com/example/tabdil/di/AppModule.kt
@@ -0,0 +1,18 @@
+package com.example.tabdil.di
+
+import com.example.tabdil.data.TabdilService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class AppModule {
+    @Singleton
+    @Provides
+    fun provideTabdilService(): TabdilService {
+        return TabdilService.create()
+    }
+}
