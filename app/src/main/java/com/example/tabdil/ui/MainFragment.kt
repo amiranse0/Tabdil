55ca7918c413b09ffa5ed371d51f32472d426068
amirabbas <amiranse0@gmail.com>
2023-04-19 03:01:14 +0430

    handle ui for different state of data added.(loading, error, success, empty error, empty loading)

--- app/src/main/java/com/example/tabdil/ui/MainFragment.kt
+++ app/src/main/java/com/example/tabdil/ui/MainFragment.kt
@@ -5,7 +5,10 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -37,6 +40,7 @@ class MainFragment: Fragment(R.layout.fragment_main) {
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.currencyStateFlow.collect{
+                    handleUiState(it)
                     if (it is ResultOf.Success){
                         putDataOnView(it.data)
                     }
@@ -53,6 +57,41 @@ class MainFragment: Fragment(R.layout.fragment_main) {
 
     fun putDataOnView(data:List<LocalCurrency>){
         mainAdapter.submitList(data)
+    }
+
+    private fun handleUiState(state: ResultOf<*>) {
+        val progressBar = activity?.findViewById<ProgressBar>(R.id.progress_bar)
+        val errorLayout = activity?.findViewById<TextView>(R.id.error_view)
+        val resultView = activity?.findViewById<ConstraintLayout>(R.id.result_view)
+        when (state) {
+            is ResultOf.ErrorEmptyLocal -> {
+                errorLayout?.visibility = View.VISIBLE
+                progressBar?.visibility = View.INVISIBLE
+                resultView?.visibility = View.INVISIBLE
+                Toast.makeText(requireContext(), getString(R.string.error_message), Toast.LENGTH_SHORT).show()
+            }
+            is ResultOf.LoadingEmptyLocal -> {
+                errorLayout?.visibility = View.INVISIBLE
+                progressBar?.visibility = View.VISIBLE
+                resultView?.visibility = View.INVISIBLE
+            }
+            is ResultOf.Success -> {
+                errorLayout?.visibility = View.INVISIBLE
+                progressBar?.visibility = View.INVISIBLE
+                resultView?.visibility = View.VISIBLE
+            }
+            is ResultOf.LoadingFillLocal -> {
+                errorLayout?.visibility = View.INVISIBLE
+                progressBar?.visibility = View.VISIBLE
+                resultView?.visibility = View.VISIBLE
+            }
+            is ResultOf.ErrorFillLocal -> {
+                errorLayout?.visibility = View.INVISIBLE
+                progressBar?.visibility = View.INVISIBLE
+                resultView?.visibility = View.VISIBLE
+                Toast.makeText(requireContext(), getString(R.string.error_message), Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 
