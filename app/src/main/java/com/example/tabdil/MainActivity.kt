581510b5a96e1a8a6215915ab6ff5d5558794932
amirabbas <amiranse0@gmail.com>
2023-04-19 18:38:53 +0430

    convert to single activity and remove the fragment.

--- app/src/main/java/com/example/tabdil/MainActivity.kt
+++ app/src/main/java/com/example/tabdil/MainActivity.kt
@@ -5,10 +5,18 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.example.tabdil.data.model.local.LocalCurrency
 import com.example.tabdil.databinding.ActivityMainBinding
+import com.example.tabdil.ui.CurrencyAdapter
+import com.example.tabdil.ui.MainViewModel
+import com.example.tabdil.ui.OnFavoriteClickListener
+import com.example.tabdil.ui.OnPinClickListener
+import com.example.tabdil.util.ResultOf
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -16,16 +24,72 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val viewModel: MainViewModel by viewModels()
+
+    private val mainAdapter: CurrencyAdapter = CurrencyAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        viewModel.getCurrencies()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.listCurrencies.adapter = mainAdapter
 
         setContentView(binding.root)
 
         setSupportActionBar(binding.myToolbar)
 
+        getData()
+
+        handelSwipeRefreshLayout()
+
+        clickFavorite()
+        clickPin()
+
     }
+
+    private fun handelSwipeRefreshLayout(){
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            viewModel.getCurrencies()
+            getData()
+            binding.swipeRefreshLayout.isRefreshing = false
+        }
+    }
+
+    private fun getData(){
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.currencyStateFlow.collect{
+                    if (it is ResultOf.Success){
+                        putDataOnView(it.data)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun clickFavorite(){
+        mainAdapter.setFavoriteClickListener(object : OnFavoriteClickListener {
+            override fun onFavoriteClick(item: LocalCurrency) {
+                viewModel.updateFavoriteOrUnfavorite(item)
+            }
+        })
+    }
+
+    private fun clickPin(){
+        mainAdapter.setPinClickListener(object : OnPinClickListener {
+            override fun onPinClick(item: LocalCurrency) {
+                viewModel.updatePinOrUnpin(item)
+            }
+        })
+    }
+
+    fun putDataOnView(data:List<LocalCurrency>){
+        mainAdapter.submitList(data)
+    }
+
 
     private fun connectHandler(disconnectedView: MenuItem) {
         lifecycleScope.launch {
@@ -61,3 +125,44 @@ class MainActivity : AppCompatActivity() {
     }
 
 }
+
+
+
+/*
+private fun handleUiState(state: ResultOf<*>) {
+        val progressBar = activity?.findViewById<ProgressBar>(R.id.progress_bar)
+        val errorLayout = activity?.findViewById<TextView>(R.id.error_view)
+        val resultView = activity?.findViewById<ConstraintLayout>(R.id.result_view)
+        when (state) {
+            is ResultOf.ErrorEmptyLocal -> {
+                errorLayout?.visibility = View.VISIBLE
+                progressBar?.visibility = View.INVISIBLE
+                resultView?.visibility = View.INVISIBLE
+                Toast.makeText(requireContext(), getString(R.string.error_message), Toast.LENGTH_SHORT).show()
+            }
+            is ResultOf.LoadingEmptyLocal -> {
+                errorLayout?.visibility = View.INVISIBLE
+                progressBar?.visibility = View.VISIBLE
+                resultView?.visibility = View.INVISIBLE
+            }
+            is ResultOf.Success -> {
+                errorLayout?.visibility = View.INVISIBLE
+                progressBar?.visibility = View.INVISIBLE
+                resultView?.visibility = View.VISIBLE
+            }
+            is ResultOf.LoadingFillLocal -> {
+                errorLayout?.visibility = View.INVISIBLE
+                progressBar?.visibility = View.VISIBLE
+                resultView?.visibility = View.VISIBLE
+            }
+            is ResultOf.ErrorFillLocal -> {
+                errorLayout?.visibility = View.INVISIBLE
+                progressBar?.visibility = View.INVISIBLE
+                resultView?.visibility = View.VISIBLE
+                Toast.makeText(requireContext(), getString(R.string.error_message), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+}
+ */
